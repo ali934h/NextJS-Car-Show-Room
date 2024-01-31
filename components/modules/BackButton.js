d325e059c9d971fe5e82151e480712315e1e6154
Ali Hosseini <ali934h@gmail.com>
2024-01-31 19:57:25 +0330

    use userRouter for back button

--- components/modules/BackButton.js
+++ components/modules/BackButton.js
@@ -1,16 +1,21 @@
 // components/modules/BackButton.js
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { IoArrowBackCircleSharp } from "react-icons/io5";
 function BackButton() {
+  const router = useRouter();
   return (
     <div className="w-9/12">
-      <Link
+      <button
+        onClick={() => {
+          router.back();
+        }}
         href="/"
         className="flex w-fit items-center justify-center gap-x-1 self-start rounded-md bg-primary px-2 py-2 text-xl font-bold"
       >
         <IoArrowBackCircleSharp className="text-3xl" />
         Back
-      </Link>
+      </button>
     </div>
   );
 }
